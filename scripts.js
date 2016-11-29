1bb2c56129eda3922d8fe8b546cce0e1db3e6c40
malikali10 <ma15us@brocku.ca>
2016-11-28 20:13:36 -0500

    Add files via upload
    java assignment

--- scripts.js
+++ scripts.js
@@ -1,10 +1,60 @@
-function getvalue(){
+var numberone;
+var numbertwo;
+var numarray= ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "twentyone", "twentytwo", "twentythree", "twentyfour", "twentyfive"];
+function calculator(){
+  numberone =document.getElementById('num1').value;
+  numbertwo =document.getElementById('num2').value;
+if (numberone =="one"){
+  numberone=1;
+}
+if (numberone =="two"){
+  numberone=2;
+}
 
-var value=document.getElementById("smile").value;
-console.log(value);
+if (numberone =="three"){
+  numberone=3;
+}
+if (numberone =="four"){
+  numberone=4
+}
+if (numberone =="five"){
+  numberone=5;
+}
 
-for (i=0;i<=value;i++){
-    console.log(i);
-    document.getElementById("happy").innerHTML += i+ "<br />";
-  }
+if (numbertwo =="one"){
+  numbertwo=1;
+}
+if (numbertwo =="two"){
+  numbertwo=2;
+}
+
+if (numbertwo =="three"){
+  numbertwo=3;
+}
+if (numbertwo =="four"){
+  numbertwo=4
+}
+if (numbertwo =="five"){
+  numbertwo=5;
+}
+}
+function add(){
+  calculator();
+  console.log(numberone);
+  var total=numberone+numbertwo;
+  document.getElementById('output').innerHTML=numarray[total];
+}
+function subtract(){
+  var total=numberone-numbertwo;
+  document.getElementById('output').innerHTML=numarray[total];
+}
+
+function multiply(){
+  var total=numberone*numbertwo;
+  document.getElementById('output').innerHTML=numarray[total];
+}
+
+function divide(){
+  var total=numberone/numbertwo;
+  document.getElementById('output').innerHTML=numarray[total];
 }
